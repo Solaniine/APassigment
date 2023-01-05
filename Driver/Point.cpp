5041b6ea1c8bc86937214f10622ab3881f21aa86
Owen Welsh <100965418+Solaniine@users.noreply.github.com>
2023-01-05 15:14:03 +0000

    Add files via upload

---
+++ Driver/Point.cpp
@@ -0,0 +1,28 @@
+#include "point.h"
+//default
+Point::Point() : x(0), y(0) 
+{
+}
+//construct for the points x and y object
+Point::Point(double x, double y) : x(x), y(y)
+{
+}
+
+
+double Point::getX() const 
+{
+	return x;
+}
+void Point::setX(double x)
+{
+	this->x = x;
+}
+
+double Point::getY() const 
+{ 
+	return y;
+}
+void Point::setY(double y) 
+{ 
+	this->y = y;
+}
