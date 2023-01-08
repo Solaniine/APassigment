b01f9ac0355e9ea4035a4f58a10a2310a370f7cf
Solaniine <25793932@students.lincoln.ac.uk>
2023-01-08 21:21:55 +0000

    blah

--- Driver/Circle.h
+++ Driver/Circle.h
@@ -1,1 +1,24 @@
-#pragma once
+#include <cmath>
+#include "Shape.h"
+#include "Movable.h"
+
+
+class Circle : public Shape, public Movable {
+
+private:
+
+    short radius;
+
+public:
+
+    Circle();Circle(int xCoordinate, int yCoordinate, float radius);
+    double calculateArea() override;
+    double calculatePerimeter() override;
+    void calculatePoints() override;
+
+    std::string toString() override;
+    void move(int newX, int newY) override; void scale(float scaleX, float scaleY) override;
+
+
+};
+
